5a74f1bb6365fb766d8bd923dc4445e01ff49bc8
SamaraF <Okami@Miyuki.fios-router.home>
2017-04-04 16:06:37 -0400

    Needs fix
    Trying to get the solver to output arrays in the right form and be
    callable by other programs

--- DiffEqSolver.py
+++ DiffEqSolver.py
@@ -14,6 +14,8 @@ Resources:
 
 from scipy.integrate import odeint
 from numpy import arange
+from numpy import append
+from numpy import array
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
@@ -23,6 +25,21 @@ def Goodwin(state,t):
     dv=v*((1/sigma)-(alpha + beta) - u/sigma)
     return [du,dv]
 
+def column(matrix, i):
+    return [row[i] for row in matrix]
+    
+def parseit(data): #return data in proper form
+    us=column(data,0)
+    vs=column(data,1)
+    length=len(us)
+    alphabeta=append(array([alpha]*length),array([beta]*length))
+    gammarho=append(array([gamma]*length),array([rho]*length))
+    abgr=append(alphabeta,gammarho)
+    cons=append(abgr,array([sigma]*length))
+    states=append(us,vs)
+    new=append(states,cons)
+    return new
+    
 variables = input("Type values for (alpha, beta, gamma, rho,sigma): \n")
 alpha, beta, gamma, rho, sigma = variables
 
@@ -30,13 +47,13 @@ t = arange(0,10,0.1) #adjust as needed
 initial=[.5,.5] #check w/ econ
 state=odeint(Goodwin, initial, t)
 
+#state is a two-dimensional array, u in the first column, v in the second
 fig1 = plt.figure()
 
 plt.plot(state[:, 0], state[:, 1], 'b-', alpha=0.2)
 plt.ylabel('V')
 plt.xlabel('U')
 #plt.show()
-
 
 def animate(i):
     plt.plot(state[0:i, 0], state[0:i, 1], 'b-')
